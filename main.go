e37ee7e0b011654c93296ad4bccfedc801933c1b
Killayt <surnamename615@gmail.com>
2022-10-18 11:44:57 +0300

    forget to add main.go

--- main.go
+++ main.go
@@ -1,32 +1,38 @@
 package main
 
 import (
-	"crypto/tls"
-
+	"fmt"
+	"log"
 	"mailerK/service/load"
+	"os"
 
 	"gopkg.in/gomail.v2"
 )
 
 func main() {
-	maket := load.GetHtmlMaket("/home/killayt/projects/Mailer-K/service/makets/fitst/user.html")
+	// registration dialer (from who we will send)
+	dialer := load.Registration()
+	fmt.Println("\n[SUCCESS] Dialer declorated by " + dialer.Username + " on host " + dialer.Host + "\n")
 
-	d := gomail.NewDialer("smtp.gmail.com", 587, "mailerk62@gmail.com", "lohzgrkpegwsffol")
-	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+	var PathToMaket []byte
+	fmt.Print("Set the full PATH to html MAKET here: ")
+	fmt.Fscan(os.Stdin, &PathToMaket)
+	maket := load.LoadMaket(string(PathToMaket))
 
-	m := gomail.NewMessage()
-	m.SetHeaders(map[string][]string{
-		"From":    {m.FormatAddress("mailerk62@gmail.com", "mailerk62")},
-		"To":      {"mailerk62@gmail.com"},
-		"Subject": {"Hello"},
-	})
-	m.SetAddressHeader("Cc", "surnamename615@gmail.com", "Maksim")
-	m.SetBody("text/html", string(maket))
+	fmt.Println("\n" + "[SUCCESS] Maket was found !")
 
-	// Send the email to Bob, Cora and Dan.
-	if err := d.DialAndSend(m); err != nil {
-		panic(err)
+	// sending
+	message := gomail.NewMessage()
+	message.SetHeader("From", dialer.Username)
+	message.SetHeader("To", dialer.Username)
+
+	// generate subject
+	subject := load.CreateSubject()
+
+	message.SetHeader("Subject", subject) //generate subject
+	message.SetBody("text/html", string(maket))
+
+	if err := dialer.DialAndSend(message); err != nil {
+		log.Panic("[ERROR]"+"not send", err)
 	}
-
-	// Send emails using d.
 }
